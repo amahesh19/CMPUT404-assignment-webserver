648dab8491fe5ac51b2a50aeb641f6e2071d97c7
Ashwin Mahesh <amahesh@cs.ualberta.ca>
2021-09-20 15:28:10 -0600

    screenshots added

--- lib.py
+++ lib.py
@@ -1,9 +1,9 @@
 lib = {
     "status_flags":{
             "success": "200 OK",
-            "http_error": 404,
-            "method_not_allowed": 405,
-            "more_possible_responses": 300,
+            "http_error": "404 Http Error",
+            "method_not_allowed": "405 Method Not Allowed",
+            "more_possible_responses": "300 More Possible Responses",
         },
     "mimetypes":{
         "text": "text/plain",
